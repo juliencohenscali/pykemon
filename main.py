11541fa65a10c84c6d08b86a8718af72d12dc405
yasss <yutop@yutop.com>
2021-10-14 23:18:03 +0200

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+from Attacks import attack
+from Pokemon import Poke
+from Player import Player
+
+
+rec = Pokemon.Poke("Vassilyx",0,"plante",100)
+print(rec.__print__())
+
+"""
+global random
+global tool
+import os
+
+os.system("ls")
+
+
+def main():
+    print("gotcha cath them all █ é")
+
+
+if __name__ == "__main__":
+    main()
